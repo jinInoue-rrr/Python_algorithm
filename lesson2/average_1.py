d092ee0c69439ef7e8f92061c8f6c4e480ee0445
jinInoue-rrr <inojin0326@icloud.com>
2021-11-24 17:46:12 +0900

    first pyfile

---
+++ lesson2/average_1.py
@@ -0,0 +1,14 @@
+##平均値を求める
+
+score = [70, 98, 92, 88, 64]
+total = 0
+for i in range(5):#index 0から4(5-1)まで
+    total = total + score[i]
+average = total/5
+print("合計点", total)
+print("平均点", average)
+
+
+
+
+
